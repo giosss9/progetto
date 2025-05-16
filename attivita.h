29cc608a272fe05757e368168590a73cf13702aa
giosss9 <g.sessa65@studenti.unisa.it>
2025-05-16 10:39:23 +0200

    Aggiunto dichiarazione della funzione confronta e definita una costante che rappresenta un item nullo

--- attivita.h
+++ attivita.h
@@ -1,4 +1,9 @@
+#include "data.h"
+#define NULLITEM NULL
+
 typedef struct attivita_studio *attivita;
 
 attivita crea_attivita(char *d, char *c, int g, int m, int a, int tempo, int pr, int st);
 void stampa_attivita(attivita a);
+
+int confronta_descrizione(attivita a, const char *descrizione);
