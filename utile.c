256b69dba519965363fbeaf5592eec0529140f10
giosss9 <g.sessa65@studenti.unisa.it>
2025-05-24 18:06:17 +0200

    Aggiunta funzione per aggiornare lo stato di un'attivita

--- utile.c
+++ utile.c
@@ -40,7 +40,7 @@ void mostra_progresso(lista l) {
             // Verifica ritardo rispetto a scadenza
             int v=confronta_data_ora(rit_scadenza(a),ottieni_data_ora());
 
-            if (v > 0) {
+            if (v < 0) {
                 printf("Stato: IN RITARDO (scadenza superata)\n");
             } else if (ore_trascorse >= stimato) {
                 printf("Stato: POSSIBILE RITARDO (tempo stimato superato)\n");
@@ -52,3 +52,47 @@ void mostra_progresso(lista l) {
         corrente = corrente->successivo;
     }
 }
+
+// Verifica se l'attività è in ritardo, se lo è cambia lo stato
+void controlla_ritardo(attivita a) {
+
+    data_ora data_corrente = ottieni_data_ora();
+    data_ora scadenza = rit_scadenza(a);
+    int confronto = confronta_data_ora(data_corrente, scadenza);
+
+    int stato=rit_stato(a);
+    if (confronto > 0 && stato != 3) {
+        imposta_stato(a,3);
+    }
+}
+
+//Funzione che riceve come argomento una struttura di tipo attivita e un intero che deve essere 1 o 2
+//Se l'attività è completata o la scelta non è valida restituisce 1, se è in ritardo restituisce 2, altrimenti restituisce 0
+int aggiorna_stato(attivita a, int scelta) {
+	// Verifica se l'attività è in ritardo prima di tutto
+    controlla_ritardo(a);
+
+    // Verifica se l'attività è già completata
+    if (rit_stato(a) == 2) {
+        printf("Attenzione! Questa attività è già completata.\n");
+        return 1;
+    }
+
+    // Controllo validità input
+    if (scelta < 1 || scelta > 2) {
+        printf("Input non valido. Inserisci 1 (in corso) o 2 (completata).\n");
+        return 1;
+    }
+
+    if (rit_stato(a) == 3 && scelta==1){
+        printf("L'attività è già in ritardo! Affrettati a completarla.\n");
+        return 2;
+    }
+    // Aggiorna lo stato
+    imposta_stato(a, scelta);
+    printf("Stato aggiornato correttamente.\n");
+    return 0;
+}
+
+
+
--- utile.h
+++ utile.h
@@ -1,1 +1,4 @@
 void mostra_progresso(lista l);
+
+void controlla_ritardo(attivita a);
+int aggiorna_stato(attivita a, int scelta);
