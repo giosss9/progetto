3543517db8a10cc5af0869bbf4b23699322650f1
giosss9 <g.sessa65@studenti.unisa.it>
2025-06-08 15:28:19 +0200

    Risolto problema di inserimento sbagliato

--- main.c
+++ main.c
@@ -11,18 +11,21 @@ int main(int argc, char *argv[]) {
     int ultimo_id = 0;
     lista l = nuova_lista();
 
-    if (argc == 2) {
-        printf("Hai specificato il file '%s'. Vuoi caricare le attività da questo file? (s/n): ", argv[1]);
-        char risposta = getchar();
-        getchar(); // per consumare il newline
+ 	if (argc == 2) {
+    	char risposta;
+    	do {
+        	printf("Hai specificato il file '%s'. Vuoi caricare le attività da questo file? (s/n): ", argv[1]);
+        	risposta = getchar();
+        	while (getchar() != '\n');
+    	} while (risposta != 's' && risposta != 'S' && risposta != 'n' && risposta != 'N');
 
-        if (risposta == 's' || risposta == 'S') {
-            l = carica_attivita_da_file(argv[1], &ultimo_id);
-        }
-        // Se risponde 'n', non caricare nulla, vai direttamente al menu
+    	if (risposta == 's' || risposta == 'S') {
+        l = carica_attivita_da_file(argv[1], &ultimo_id);
     }
+    // Se risponde 'n' o 'N', va direttamente al menu
+	}
 
-    // Se non c’è file o risposta è 'n', parte comunque il menu con lista (vuota o caricata)
+
     menu(l, &ultimo_id);
 
     return 0;
